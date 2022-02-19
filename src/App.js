edfda3ba439b76935559c6a0243cbc90938df881
katerina2018 <tvorchestvo15@gmail.com>
2022-02-19 09:09:36 +0200

    create app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+import './App.css';
+import Section from './components/Section';
+// import Form from './components/Form';
+
+// import IconButton from './components/IconButton';
+// import { ReactComponent as AddIcon } from './icons/person_add_icon.svg';
+// import Modal from './components/Modal';
+
+class App extends Component {
+  // state = {
+
+  // };
+  // componentDidMount() {
+
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+
+  // }
+
+  render() {
+    return (
+      <Section>
+        <h1>Image finder</h1>
+      </Section>
+    );
+  }
+}
+
+export default App;
